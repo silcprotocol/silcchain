b8d9140e666b096df05ecaabb77a38781277d073
Puneet <puneetsingh10091998@gmail.com>
2025-04-21 19:00:59 +0530

    added configuration files to start the network for remove the inflationary model from silc protocol

---
+++ app/baseapp.go
@@ -0,0 +1,28 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package app
+
+import (
+	"errors"
+	"io"
+)
+
+// Close will be called in graceful shutdown in start cmd
+func (app *Silc) Close() error {
+	errs := []error{app.BaseApp.Close()}
+
+	// flush the versiondb
+	if closer, ok := app.qms.(io.Closer); ok {
+		errs = append(errs, closer.Close())
+	}
+
+	// mainly to flush memiavl
+	if closer, ok := app.BaseApp.CommitMultiStore().(io.Closer); ok {
+		errs = append(errs, closer.Close())
+	}
+
+	err := errors.Join(errs...)
+	app.BaseApp.Logger().Info("Application gracefully shutdown", "error", err)
+	return err
+}
